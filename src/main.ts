ecb99f6e47a502c37df57b7f8801acfb0d8204e4
Biplob <biplob.kazsoft@gmail.com>
2023-03-28 14:38:16 +0600

    list is immutable when memorize

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { List } from "immutable";
+
+type Sum = (arr: List<number>) => number;
+
+const memo = (fn: Sum): Sum => {
+  let prevArr: List<number>;
+  let prevRes: number;
+  return (arr) => {
+    return arr === prevArr
+      ? prevRes
+      : ((prevArr = arr), (prevRes = fn.call(this, arr)));
+  };
+};
+
+const sum: Sum = (arr) => {
+  let i = 0;
+  while (i < 3423434423) {
+    i++;
+  }
+  return arr.reduce((a, b) => a + b, 0);
+};
+
+export const main = () => {
+  const memoSum = memo(sum);
+  let arr = List([2, 3, 4]);
+  const res = memoSum(arr);
+  console.log(res);
+  arr = arr.push(6);
+  const res2 = memoSum(arr);
+  console.log(res2);
+};
+
+main();
